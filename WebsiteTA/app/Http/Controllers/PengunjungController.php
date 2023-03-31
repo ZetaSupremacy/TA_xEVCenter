455a2ca17ee657271515c1ec86b253e6a2df07b5
ZetaSupremacy <127097010+ZetaSupremacy@users.noreply.github.com>
2023-04-01 00:28:39 +0700

    CRUD Done

--- WebsiteTA/app/Http/Controllers/PengunjungController.php
+++ WebsiteTA/app/Http/Controllers/PengunjungController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use App\Models\pengunjung;
 
 class PengunjungController extends Controller
@@ -13,9 +14,18 @@ class PengunjungController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('pengunjung.index');
+        $katakunci = $request->$katakunci;
+        if(strlen($katakunci)){
+            $data = pengunjung::where('nama', 'like', "%$katakunci%")
+                    ->orWhere('nohp', 'like', "%$katakunci%")
+                    ->orWhere('email', 'like', "%$katakunci%");
+        } else {
+            $data = pengunjung::orderBy('id','desc')->get();
+        }
+        
+        return view('pengunjung.index')->with('data', $data);
     }
 
     /**
@@ -36,6 +46,29 @@ class PengunjungController extends Controller
      */
     public function store(Request $request)
     {
+        Session::flash('nama',$request->nama);
+        Session::flash('nohp',$request->nohp);
+        Session::flash('alamat',$request->alamat);
+        Session::flash('email',$request->email);
+        Session::flash('lembaga',$request->lembaga);
+        Session::flash('namalembaga',$request->namalembaga);
+
+        $request->validate([
+            'nama'=>'required:pengunjung,nama',
+            'nohp'=>'required|numeric|:pengunjung,nohp',
+            'alamat'=>'required:pengunjung,alamat',
+            'email'=>'required:pengunjung,email',
+            'lembaga'=>'required:pengunjung,lembaga',
+            'namalembaga'=>'required:pengunjung,namalembaga',
+        ],[
+            'nama.required'=>'Nama Wajib Diisi!',
+            'nohp.required'=>'Nomer Handphone Wajib Diisi!',
+            'nohp.numeric'=>'Nomer Handphone Wajib Dalam Angka!',
+            'alamat.required'=>'Alamat Wajib Diisi!',
+            'email.required'=>'Email Wajib Diisi!',
+            'lembaga.required'=>'Lembaga Wajib Dipilih!',
+            'namalembaga.required'=>'Nama Lembaga Wajib Diisi!',
+        ]);
         $data = [
             'nama'=>$request->nama,
             'nohp'=>$request->nohp,
@@ -45,6 +78,7 @@ class PengunjungController extends Controller
             'namalembaga'=>$request->namalembaga,
         ];
         pengunjung::create($data);
+        return redirect()->to('pengunjung')->with('success', 'Terimakasih Sudah Mendaftarkan Diri Untuk Mengunjungi xEV Center, Kami Tunggu Kehadiran Anda :)');
     }
 
     /**
@@ -66,7 +100,8 @@ class PengunjungController extends Controller
      */
     public function edit($id)
     {
-        //
+        $data = pengunjung::where('nama', $id)->first();
+        return view('pengunjung.edit')->with ('data', $data);
     }
 
     /**
@@ -78,7 +113,32 @@ class PengunjungController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'nama'=>'required:pengunjung,nama',
+            'nohp'=>'required|numeric|:pengunjung,nohp',
+            'alamat'=>'required:pengunjung,alamat',
+            'email'=>'required:pengunjung,email',
+            'lembaga'=>'required:pengunjung,lembaga',
+            'namalembaga'=>'required:pengunjung,namalembaga',
+        ],[
+            'nama.required'=>'Nama Wajib Diisi!',
+            'nohp.required'=>'Nomer Handphone Wajib Diisi!',
+            'nohp.numeric'=>'Nomer Handphone Wajib Dalam Angka!',
+            'alamat.required'=>'Alamat Wajib Diisi!',
+            'email.required'=>'Email Wajib Diisi!',
+            'lembaga.required'=>'Lembaga Wajib Dipilih!',
+            'namalembaga.required'=>'Nama Lembaga Wajib Diisi!',
+        ]);
+        $data = [
+            'nama'=>$request->nama,
+            'nohp'=>$request->nohp,
+            'alamat'=>$request->alamat,
+            'email'=>$request->email,
+            'lembaga'=>$request->lembaga,
+            'namalembaga'=>$request->namalembaga,
+        ];
+        pengunjung::where('nama' ,$id)->update($data);
+        return redirect()->to('pengunjung')->with('success', 'Data Telah Berhasil Diubah');
     }
 
     /**
@@ -89,6 +149,7 @@ class PengunjungController extends Controller
      */
     public function destroy($id)
     {
-        //
+        pengunjung::where('nama', $id)->delete();
+        return redirect()->to('pengunjung')->with('success', 'Berhasil Menghapus Data');
     }
 }
