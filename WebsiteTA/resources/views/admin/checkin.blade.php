24a5c3cd4d45780332027eed13ec1d88b3333a7e
Nerds122 <133510630+Nerds122@users.noreply.github.com>
2023-06-13 20:42:07 +0700

    Update checkin.blade.php

--- WebsiteTA/resources/views/admin/checkin.blade.php
+++ WebsiteTA/resources/views/admin/checkin.blade.php
@@ -1,6 +1,14 @@
-@extends('layouts.navbar_admin')
 
-    @section('content')
+<!doctype html>
+<html lang="en">
+    <head>  
+        <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    </head>
+
+    <body>
         
 @if (session('message'))
 <div class="alert alert-info alert-dismissible fade show" role="alert">
@@ -86,7 +94,7 @@
           
         <div class="mx-auto" style="width:400px; height:400px">
             <div id="reader" width="200px"></div>
-            </div>
+          </div>
         </div>
         
       </div>
@@ -94,5 +102,44 @@
   </div>
 
         </form>
+    </body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script> 
+    let isScanned = false;
+    let html5QrcodeScanner;
+    $('#exampleModal').on('show.bs.modal', function (event) {
+
+        let html5QrcodeScanner = new Html5QrcodeScanner(
+          "reader",
+          { fps: 10, qrbox: {width: 250, height: 250} },
+          /* verbose= */ false);
+          
+        html5QrcodeScanner.render(onScanSuccess);
     
-@endsection
+        function onScanSuccess(decodedText, decodedResult) {
+          // handle the scanned code as you like, for example:
+          if (!isScanned) {
+            isScanned = true; 
+            $('#booking_code').val(decodedText);
+            document.forms[0].submit();
+          }
+        }
+
+        $('#exampleModal').on('hidden.bs.modal', function (event) {
+    // Menghentikan pemindaian QR code
+    if (html5QrcodeScanner) {
+        html5QrcodeScanner.clear(); // Membersihkan layar scanner
+        html5QrcodeScanner.stop(); // Menghentikan pemindaian
+        isScanned = false; // Setel ulang status pemindaian
+    }
+});
+
+    });
+    
+
+    </script>
+
+       </html>
